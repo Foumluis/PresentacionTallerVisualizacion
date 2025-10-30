ec92faae2b9b62888eaabe0e5a8881980b8b801d
Luis-Felipe Caceres <luiscaceceresalarcon@outlook.com>
2025-10-30 12:56:07 -0300

    Pandas y numpy en views.py

--- ZZ/app/views.py
+++ ZZ/app/views.py
@@ -1,10 +1,102 @@
 from django.shortcuts import render
-
+from django.conf import settings
+import pandas as pd
+import numpy as np
 # Create your views here.
+
+csv_path = f"{settings.BASE_DIR}/app/static/excel.csv"
+tarea = pd.read_csv(csv_path)
+tarea = tarea.set_index("Customer ID")
+#------------------------------------------------------------------------------------------
+
+#Value_counts de los valores categoricos
+categorias = []
+for i in ['Gender', 'Item Purchased', 'Category','Location', 'Size', 'Color', 'Season','Subscription Status', 'Shipping Type', 'Discount Applied','Promo Code Used', 'Payment Method','Frequency of Purchases']:
+    categorias.append(tarea[i].value_counts())
+#------------------------------------------------------------------------------------------
+
+#Viendo los nulos por columna
+nulos = tarea.isnull().sum()
+#------------------------------------------------------------------------------------------
+
+# funcion que traforma los yes en 0 y los no en 1
+#------------------------------------------------------------------------------------------
+
+tarea['Gender']=np.where(tarea['Gender'] == 'Male', 'Hombre',tarea['Gender'])
+tarea['Gender']=np.where(tarea['Gender'] == 'Female', 'Mujer',tarea['Gender'])
+
+
+
+# Traducción manual de valores categóricos
+# Item Purchased
+tarea['Item Purchased'] = tarea['Item Purchased'].replace({
+    'Shirt': 'Camisa',
+    'Pants': 'Pantalón',
+    'Shoes': 'Zapatos',
+    'Dress': 'Vestido',
+    'Hat': 'Sombrero',
+    'Jacket': 'Chaqueta',
+    'Skirt': 'Falda',
+    'Shorts': 'Shorts',
+    'Sweater': 'Suéter',
+    'Socks': 'Calcetines',
+    'Sandals': 'Sandalias',
+    'Boots': 'Botas'
+})
+
+
+
+# Category
+tarea['Category'] = tarea['Category'].replace({
+    'Clothing': 'Ropa',
+    'Footwear': 'Calzado',
+    'Accessories': 'Accesorios'
+})
+
+# Color
+tarea['Color'] = tarea['Color'].replace({
+    'Red': 'Rojo',
+    'Blue': 'Azul',
+    'Green': 'Verde',
+    'Black': 'Negro',
+    'White': 'Blanco',
+    'Yellow': 'Amarillo',
+    'Pink': 'Rosa',
+    'Purple': 'Morado',
+    'Brown': 'Marrón',
+    'Orange': 'Naranja'
+})
+# Season
+tarea['Season'] = tarea['Season'].replace({
+    'Spring': 'Primavera',
+    'Summer': 'Verano',
+    'Fall': 'Otoño',
+    'Winter': 'Invierno'
+})
+# Payment Method
+tarea['Payment Method'] = tarea['Payment Method'].replace({
+    'Credit Card': 'Tarjeta de Crédito',
+    'Debit Card': 'Tarjeta de Débito',
+    'Cash': 'Efectivo',
+    'Bank Transfer': 'Transferencia Bancaria'
+})
+# Frequency of Purchases
+tarea['Frequency of Purchases'] = tarea['Frequency of Purchases'].replace({
+    'Weekly': 'Semanal',
+    'Monthly': 'Mensual',
+    'Bi-Weekly': 'Cada dos semanas',
+    'Fortnightly': 'Quincenal',
+    'Every 3 Months': 'Cada 3 Meses',
+    'Quarterly': 'Trimestral',
+    'Annually': 'Anual'
+})
+
 
 # Análisis Univariado
 def distribucion_clientes_genero(request):
-    return render(request, 'analisis/univariado/distribucion_genero.html')
+    x = tarea['Gender'].value_counts().index.tolist()
+    y = tarea["Gender"].value_counts().tolist()
+    return render(request, 'analisis/univariado/distribucion_genero.html', {'x': x, 'y': y})
 
 # Análisis Bivariado
 def comparacion_compras_generos(request):
